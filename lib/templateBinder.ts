f8181272a1d9f720a9dd11ba19208e550a7ff7f3
Jed Mao <jed.hunsakergmail>
2013-12-19 10:33:54 -0600

    Fix template binder

--- lib/templateBinder.ts
+++ lib/templateBinder.ts
@@ -3,12 +3,13 @@
 import express = require('express');
 var extend = require('extend');
 import base = require('../engines/base');
+import Promises = require('./Promises');
+var Deferred = Promises.Deferred;
 
 
-export function bindTemplates(request: Request, response: Response) {
+export function bindTemplates(request: Request, response: Response): Promises.Promise {
 	var templateBinder = new TemplateBinder(response);
-	templateBinder.bindTemplates(request.body);
-	return templateBinder.boundTemplates;
+	return templateBinder.bindTemplates(JSON.parse(request.body));
 }
 
 export interface Request {
@@ -26,121 +27,117 @@ interface BindRequest {
 	id?: string;
 	source?: string;
 	templates?: BindRequest[];
+	binding?: Promises.Deferred;
 }
 
 class TemplateBinder implements BindRequest {
-	private _engines = {};
-	private _isValid: boolean;
-	private _bindRequest: BindRequest;
-	engine: string;
-	data: any;
-	id: string;
-	source: string;
-	boundTemplates = {};
+	private engines: { [key: string]: typeof base.TemplatingEngine } = {};
+	private boundTemplates: { [key: string]: string } = {};
 
-	// ReSharper disable once InconsistentNaming
-	constructor(private _response: Response) {
+	constructor(private response: Response) {
 	}
 
-	public bindTemplates(bindRequest: BindRequest) {
-		this._bindRequest = bindRequest;
+	public bindTemplates(context: BindRequest): Promises.Promise {
 
-		var templates = bindRequest.templates;
-		if (templates) {
-			saveState.call(this);
-			templates.forEach(this.bindTemplates.bind(this));
-			restoreState.call(this);
-			return;
+		var binding = new Deferred();
+		var binders = [];
+
+		if (context.engine && context.source && context.data) {
+			var d = new Deferred();
+			binders.push(d.promise);
+			// ReSharper disable once InconsistentNaming
+			this.tryLoadingEngine(context).then((Engine: typeof base.TemplatingEngine) => {
+				new Engine().compile(context.source).done(template => {
+					template.render(context.data || {}).done(html => {
+						this.boundTemplates[context.id] = html;
+						d.resolve();
+					});
+				});
+			}, d.reject.bind(context.binding));
 		}
 
-		// ReSharper disable MisuseOfOwnerFunctionThis
-		function saveState() {
-			this.savedState = {
-				_bindRequest: this._bindRequest,
-				data: this.data,
-				engine: this.engine
-			};
+		if (context.templates) {
+			context.templates.forEach(template => {
+				binders.push(this.bindTemplates(extend({
+					engine: context.engine,
+					data: context.data
+				}, template)));
+			});
 		}
 
-		function restoreState() {
-			this._bindRequest = bindRequest;
-			this.data = this.savedState.data;
-			this.engine = this.savedState.engine;
-		}
-		// ReSharper restore MisuseOfOwnerFunctionThis
+		Promises.when.apply(this, binders).done(() => {
+			binding.resolve(this.boundTemplates);
+		});
 
-		this.validate();
-		if (this._isValid) {
-			this.compile();
-		}
+		return binding.promise;
 	}
 
-	private validate() {
-		var requireMethods = [
-			this.requireId,
-			this.requireSource,
-			this.requireData,
-			this.requireEngine
-		];
-		this._isValid = true;
-		for (var i = 0; i < requireMethods.length; i++) {
-			requireMethods[i].call(this);
-			if (!this._isValid) {
-				break;
+	private onBindComplete(id: string, html: string): void {
+		this.boundTemplates[id] = html;
+	}
+
+	private tryLoadingEngine(context: BindRequest): Promises.Promise {
+		var loading = new Deferred();
+		var engineName = context.engine;
+		setTimeout(() => {
+			try {
+				loading.resolve(this.engines[engineName] ||
+				    require('../engines/' + engineName));
+			} catch (e) {
+				loading.reject('unsupported template engine');
 			}
-		}
+		});
+		return loading.promise;
 	}
 
-	private requireId() {
-		this.id = this._bindRequest.id;
-		if (!this.id) {
-			this.invalidate('missing required template id');
-		}
-	}
+	//private validate(): boolean {
+	//	var requireMethods = [
+	//		this.requireId,
+	//		this.requireSource,
+	//		this.requireData,
+	//		this.requireEngine
+	//	];
+	//	var valid = true;
+	//	for (var i = 0; i < requireMethods.length; i++) {
+	//		valid = requireMethods[i].call(this, valid);
+	//		if (!valid) {
+	//			break;
+	//		}
+	//	}
+	//	return valid;
+	//}
+
+	//private requireId(id: string) {
+	//	if (!id) {
+	//		this.invalidate('missing required template id');
+	//	}
+	//}
 
 	private invalidate(message: string) {
-		this._response.send(400, message);
-		this._isValid = false;
+		this.response.send(400, message);
 	}
 
-	private requireSource() {
-		this.source = this._bindRequest.source || this.source;
-		if (!this.source) {
-			this.invalidate('missing required template source');
-		}
-	}
+	//private requireSource() {
+	//	this.source = this._bindRequest.source || this.source;
+	//	if (!this.source) {
+	//		this.invalidate('missing required template source');
+	//	}
+	//}
 
-	private requireData() {
-		this.data = extend(this._bindRequest.data, this.data || {});
-		if (!this.data) {
-			this.invalidate('missing required template data');
-		}
-	}
+	//private requireData() {
+	//	this.data = extend(this._bindRequest.data, this.data || {});
+	//	if (!this.data) {
+	//		this.invalidate('missing required template data');
+	//	}
+	//}
 
-	private requireEngine() {
-		var name = this._bindRequest.engine;
-		if (name) {
-			this.tryLoadingEngine(name);
-		}
-		if (!this.engine) {
-			this.invalidate('unspecified templating engine');
-		}
-	}
-
-	private tryLoadingEngine(name: string) {
-		try {
-			this._engines[name] = require('../engines/' + name);
-		} catch (e) {
-			this.invalidate('unsupported templating engine');
-		}
-	}
-
-	private compile() {
-		this._engines[this.engine].compile(this.source)
-			.done(this.addTemplate.bind(this));
-	}
-
-	private addTemplate(template: (data: any) => string) {
-		this.boundTemplates[this.id] = template(this.data);
-	}
+	//private requireEngine() {
+	//	var name = this._bindRequest.engine;
+	//	if (name) {
+	//		this.tryLoadingEngine(name);
+	//	}
+	//	if (!this.engine) {
+	//		this.invalidate('unspecified templating engine');
+	//	}
+	//}
 }
--- test/lib/templateBinder.ts
+++ test/lib/templateBinder.ts
@@ -9,7 +9,6 @@ import sinon = require('sinon');
 
 var response: MiniResponse;
 var send: SinonSpy;
-var json: SinonSpy;
 
 export class MiniResponse implements templateBinder.Response {
 	send(statusOrBody: any, body?: any): MiniResponse {
@@ -23,17 +22,39 @@ export class MiniResponse implements templateBinder.Response {
 beforeEach(() => {
 	response = new MiniResponse();
 	send = sinon.spy(response, 'send');
-	json = sinon.spy(response, 'json');
 });
 
 describe('Template Binder', () => {
 
 	it('handles basic template binding', done => {
-		var boundTemplates = templateBinder.bindTemplates({
-			body: 'foo'
-		}, response);
-		response.json(boundTemplates);
-		done();
+		templateBinder.bindTemplates({
+			body: JSON.stringify({
+				engine: 'hbs',
+				id: 'one',
+				source: '{{foo}}',
+				data: {
+					foo: 'bar',
+					bar: 'baz',
+					baz: 'qux'
+				},
+				templates: [
+					{
+						id: 'two',
+						source: '{{bar}}'
+					},
+					{
+						id: 'three',
+						source: '{{baz}}'
+					}
+				]
+			})
+		}, response).done(json => {
+			expect(json.one).to.equal('bar');
+			expect(json.two).to.equal('baz');
+			expect(json.three).to.equal('qux');
+			//expect(send).to.have.been.calledWithExactly(400, 'message');
+			done();
+		});
 	});
 
 });
